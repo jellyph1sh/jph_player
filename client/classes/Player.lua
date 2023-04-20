9c18d8421671aa8392d4671dfdcb6def82bb1938
BuildTools <unconfigured@null.spigotmc.org>
2023-04-20 16:47:33 +0200

    :construction: | Class player

--- client/classes/Player.lua
+++ client/classes/Player.lua
@@ -11,5 +11,33 @@ function Player:new(name, surname, age)
     self.thirst = 100
     self.inventory = Inventory:new(Config.InventorySlots)
     self.account = Account:new(Config.DefaultBalance)
+    self.Starve()
     return player
+end
+
+function Player:getHunger()
+    return self.hunger
+end
+
+function Player:setHunger(amount)
+    self.hunger = amount
+end
+
+function Player:getThirst()
+    return self.thirst
+end
+
+function Player:setThirst(amount)
+    self.thirst = amount
+end
+
+function Player:Starve()
+    Citizen.CreateThread(function()
+        while true do
+            self.setHunger(self.getHunger() - Config.StarveHunger)
+            self.setThirst(self.getThirst() - Config.StarveThirst)
+
+            Citizen.Wait(1000)
+        end
+    end)
 end
--- config.lua
+++ config.lua
@@ -2,3 +2,6 @@ Config = {}
 
 Config.InventorySlots = 5
 Config.DefaultBalance = 500
+
+Config.StarveHunger = 0.025
+Config.StarveThirst = 0.035
