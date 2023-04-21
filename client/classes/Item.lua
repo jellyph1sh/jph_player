e771c843d6dbe1d84ccc5703e7bdca2a48746f96
BuildTools <unconfigured@null.spigotmc.org>
2023-04-21 15:43:56 +0200

    :construction: | Create child for items.

--- client/classes/Item.lua
+++ client/classes/Item.lua
@@ -8,3 +8,23 @@ function Item:new(name, model)
     self.model = model
     return item
 end
+
+MoneyItem = {name = "", model = "", value = 1}
+
+function MoneyItem:new(name, model, value)
+    local moneyItem = Item:new(name, model)
+    setmetatable(moneyItem, self)
+    self.__index = self
+    self.value = value
+    return MoneyItem
+end
+
+function MoneyItem:Drop(value)
+    self.value = self.value - value
+    local plyPed = GetPlayerPed(-1)
+    local plyPos = GetEntityCoords(plyPed, true)
+    local moneyPickup = CreateMoneyPickups(plyPos, self.value, 1, GetHashKey(self.model))
+    if (value == 0) then
+        MoneyItem = nil
+    end
+end
